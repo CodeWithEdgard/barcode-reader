ea1e9425d71c8ebde41a9c0c6eb554b10034df60
Edgar Mendes <mendesbr92@gmail.com>
2026-01-11 15:31:07 -0300

    feat: criado controller inicial para conectar com fxml

---
+++ src/main/java/com/br/controller/EstoqueController.java
@@ -0,0 +1,13 @@
+package com.br.controller;
+
+import javafx.fxml.FXML;
+
+public class EstoqueController {
+
+    @FXML
+    public void initialize() {
+
+        System.out.println("Estoque controller carregado com sucesso");
+    }
+
+}
